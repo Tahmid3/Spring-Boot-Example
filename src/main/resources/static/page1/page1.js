3c6a6ac6b6cc77b45bc0cc79c26af807cc0794d1
Theissen, Phillip <phillip.theissen@accenture.com>
2019-10-24 16:16:24 +0200

    Content update

--- src/main/resources/static/page1/page1.js
+++ src/main/resources/static/page1/page1.js
@@ -14,10 +14,39 @@ app.controller("Page1Ctrl", function ($http) {
             lastName: lastName
         };
         $http.post("/newUser", newUser).then(function () {
-            console.log("Success")
+            controller.success();
+            controller.getAllUsers();
+            console.log("Success");
+            $('#firstName').val("");
+            $('#lastName').val("");
         }, function (reason) {
             console.log("Error")
         })
+    };
+
+    controller.getAllUsers = function () {
+        $http.get("/getAllUsers").then(function (value) {
+            controller.users = value.data;
+        }, function (reason) {
+            window.alert("Error");
+        })
+    };
+
+    controller.delUser = function (firstName) {
+        console.log(firstName);
+        $http.put("/delUser/" + firstName).then(function () {
+            controller.getAllUsers();
+        }, function (reason) {
+            window.alert("Error");
+        })
+    };
+
+    controller.success = function () {
+        $("#alert").append("<div class='alert alert-success alert-dismissible'>" +
+            "<strong>Success!</strong> User has been added</div>");
+        setTimeout(function () {
+            $("#alert").empty();
+        }, 4000)
     };
 
 });
--- src/main/resources/static/page2/page2.js
+++ src/main/resources/static/page2/page2.js
@@ -4,24 +4,4 @@ app.controller("Page2Ctrl", function ($http) {
     var controller = this;
     controller.users = [];
 
-    controller.loadCon = function () {
-        controller.getAllUsers();
-    };
-
-    controller.getAllUsers = function () {
-        $http.get("/getAllUsers").then(function (value) {
-            controller.users = value.data;
-        }, function (reason) {
-            window.alert("Error");
-        })
-    };
-
-    controller.delUser = function (firstName) {
-        console.log(firstName);
-        $http.put("/delUser/" + firstName).then(function () {
-            controller.loadCon();
-        }, function (reason) {
-            window.alert("Error");
-        })
-    };
 });
