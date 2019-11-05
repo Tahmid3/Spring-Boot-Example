4a2922976d84144e91da637070b1d9ae653e477b
tahmid3 <shibly.tahmid@gmail.com>
2019-11-05 20:03:51 +0100

    h2 in mem

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -2,7 +2,9 @@ package com.example.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Entity;
 
+@Entity
 @Data
 @NoArgsConstructor
 public class User {
