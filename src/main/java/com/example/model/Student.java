6556a5f9197c099ebf9ff0146e698977810688c6
tahmid3 <shibly.tahmid@gmail.com>
2019-11-07 08:40:34 +0100

    changed db settings

---
+++ src/main/java/com/example/model/Student.java
@@ -0,0 +1,12 @@
+package com.example.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String passportNumber;
+}
