9c48fc41c9749c16904005cd71a739d28d386ce2
tahmid3 <shibly.tahmid@gmail.com>
2019-11-12 23:16:39 +0100

    Revert "h2 in mem"
    This reverts commit 4a2922976d84144e91da637070b1d9ae653e477b.
    
    # Conflicts:
    #	src/main/resources/application.properties
    
    Running

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -2,9 +2,7 @@ package com.example.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Entity;
 
-@Entity
 @Data
 @NoArgsConstructor
 public class User {
